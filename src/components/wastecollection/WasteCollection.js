346e79ae4aee3aeaba09f1aa2dafd70e00d93b20
narsenico <not_amon-dev@yahoo.it>
2020-07-24 23:24:10 +0200

    Lettura info raccolta rifiuti

--- src/components/wastecollection/WasteCollection.js
+++ src/components/wastecollection/WasteCollection.js
@@ -1,9 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+
+import { formatDate, shortTime, parseDate, humanDate } from '../../utility';
 
 import './WasteCollection.css';
 
 const SERVER_BASE_URL = process.env.REACT_APP_SERVER_BASE_URL;
+// ora di termine ritiro rifiuti
+const WASTE_COLLECTION_END_TIME = '10:00';
+// descrizione tipologia rifiuti
+const WASTE_DESCR = {
+    P: 'Plastica',
+    C: 'Carta',
+    U: 'Umido',
+    V: 'Vetro',
+    S: 'Secco',
+};
 
 /**
  * Componente che mostra il prossimo ritiro dei rifiuti.
@@ -11,21 +23,73 @@ const SERVER_BASE_URL = process.env.REACT_APP_SERVER_BASE_URL;
  * TODO: aggiungere link a editor (importare da waste-collection-calendar)
  */
 function WasteCollection() {
+    const [waste, setWaste] = useState({});
+
     useEffect(() => {
         (async () => {
             try {
+                const today = formatDate(new Date(), 'yyyyMMdd');
+                const now = shortTime();
+                // ritorna il ritiro per la data indicata (sempre) e il successivo (se c'è)
                 const res = await axios.get(
-                    `${SERVER_BASE_URL}/waste`
+                    `${SERVER_BASE_URL}/waste/${today}`
                 );
-                const waste = res.data;
-                console.log(waste);
+                // ritorna un array
+                // come primo elemento la data richiesta
+                // come seoncdo il ritiro successivo alla data richiesta, se esiste
+                const { data } = res;
+                // console.log(data);
+
+                // se non è ancora passata l'ora del ritiro cerco oggi
+                const index =
+                    now <= WASTE_COLLECTION_END_TIME && data[0] ? 0 : 1;
+                if (data[index]) {
+                    // data[index].waste = 'CVP';
+                    setWaste({
+                        title: humanDate(parseDate(data[index].date)), // TODO: formattare
+                        items: data[index].waste.split('').map((wt) => ({
+                            icon: 'waste-icon-' + wt,
+                            type: wt,
+                            description: WASTE_DESCR[wt],
+                        })),
+                    });
+                } else {
+                    setWaste({
+                        title: 'NULLA',
+                        itmes: []
+                    })
+                }
             } catch (e) {
                 console.error(e);
             }
         })();
     }, []);
 
-    return <div className="waste-collection">WASTE COLLECTION</div>;
+    return (
+        <div className="waste-collection box">
+            <div className="waste-collection-description box-header text-center place-center text-uppercase text-ellipsis text-2x">
+                {waste.title}
+            </div>
+            <div className="waste-collection-items">
+                {waste.items &&
+                    waste.items.map((item, index) => (
+                        <div key={index}>
+                            <div
+                                className="waste-collection-icon"
+                            >
+                                <div
+                                    className="waste-icon"
+                                    type={item.type}
+                                ></div>
+                            </div>
+                            <div className="waste-collection-descr text-uppercase">
+                                {item.description}
+                            </div>
+                        </div>
+                    ))}
+            </div>
+        </div>
+    );
 }
 
 export default WasteCollection;
